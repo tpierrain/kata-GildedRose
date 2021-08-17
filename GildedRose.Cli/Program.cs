43362ded2ad9253316a9465d7b8353cdf0f4a1b7
tpierrain <thomas@pierrain.net>
2021-08-17 18:33:30 +0200

    Migrate solution to .NET core 5

--- GildedRose.Cli/Program.cs
+++ GildedRose.Cli/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace GildedRose.Console
+namespace GildedRose.Cli
 {
     class Program
     {
@@ -11,23 +11,23 @@ namespace GildedRose.Console
             System.Console.WriteLine("OMGHAI!");
 
             var app = new Program()
-                          {
-                              Items = new List<Item>
+            {
+                Items = new List<Item>
                                           {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                                              new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
+                                              new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+                                              new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
+                                              new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
                                               new Item
                                                   {
                                                       Name = "Backstage passes to a TAFKAL80ETC concert",
                                                       SellIn = 15,
                                                       Quality = 20
                                                   },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                                              new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
                                           }
 
-                          };
+            };
 
 
             System.Console.WriteLine("Type <enter> to pass to the next day or <esc> to quit:");
@@ -132,5 +132,4 @@ namespace GildedRose.Console
 
         public int Quality { get; set; }
     }
-
 }
