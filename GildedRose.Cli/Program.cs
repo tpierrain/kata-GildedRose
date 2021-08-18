04203579c7873b2c6bcc2e81c8e2636053214547
tpierrain <thomas@pierrain.net>
2021-08-18 16:07:44 +0200

    Add test harness

--- GildedRose.Cli/Program.cs
+++ GildedRose.Cli/Program.cs
@@ -5,29 +5,18 @@ namespace GildedRose.Cli
 {
     class Program
     {
-        IList<Item> Items;
+        private readonly Inn _inn = new Inn();
+
+        public Inn Inn
+        {
+            get { return _inn; }
+        }
+
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var app = new Program()
-            {
-                Items = new List<Item>
-                                          {
-                                              new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
-                                              new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
-                                              new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
-                                              new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
-                                          }
-
-            };
+            var app = new Program();
 
 
             System.Console.WriteLine("Type <enter> to pass to the next day or <esc> to quit:");
@@ -36,89 +25,12 @@ namespace GildedRose.Cli
             while (System.Console.ReadKey().Key != ConsoleKey.Escape)
             {
                 System.Console.WriteLine("Go to next day...");
-                app.UpdateQuality();
+                app.Inn.UpdateQuality();
             }
 
             System.Console.WriteLine("Ciao...");
             System.Console.ReadLine();
         }
-
-        public void UpdateQuality()
-        {
-            for (var i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
     }
 
     /// <summary>
---
+++ GildedRose.Tests/InnShould.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using GildedRose.Cli;
+using NFluent;
+
+namespace GildedRose.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class InnShould
+    {
+        [Test]
+        public void UpdateQuality_as_expected()
+        {
+            var inn = new Inn();
+            var legacyInn = new LegacyInn();
+
+            for (var i = 0; i < 18; i++)
+            {
+                Check.That(inn.Items.Select(i => i.Name)).ContainsExactly(legacyInn.Items.Select(i => i.Name));
+                Check.That(inn.Items.Select(i => i.SellIn)).ContainsExactly(legacyInn.Items.Select(i => i.SellIn));
+                Check.That(inn.Items.Select(i => i.Quality)).ContainsExactly(legacyInn.Items.Select(i => i.Quality));
+
+                inn.UpdateQuality();
+                legacyInn.UpdateQuality();
+            }
+        }
+    }
+}
